1c2ec9d635a5036718e89c810e754421b096aef6
wzkwzk1998 <869693441@qq.com>
2023-03-07 23:53:20 +0800

    add llff_fern

--- ext_experiments/single_view_diffusion_sr_x0.py
+++ ext_experiments/single_view_diffusion_sr_x0.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     cond_img = Image.open(img_path).convert('RGB')
     cond_img = torch.tensor(np.array(cond_img) / 255.0).to(device=device)
 
-    pred_rgb = (torch.ones((1, 3, 512, 512)) / 2.0).to(device=device).requires_grad_(True)
+    pred_rgb = (torch.zeros((1, 3, 512, 512))).to(device=device).requires_grad_(True)
     # pred_rgb = Image.open('./test_imgs/llff_flower_upsampling.png').convert('RGB')
     # pred_rgb = torch.tensor(np.array(pred_rgb) / 255.0).to(device=device)
     # pred_rgb = pred_rgb[None].permute(0, 3, 1, 2).to(dtype=torch.float32, device=device).requires_grad_(True)
@@ -65,7 +65,7 @@ if __name__ == '__main__':
             pred_rgb_detach.save(save_path)
             x0 = x0.detach().cpu().permute(0, 2, 3, 1).float().numpy()
             x0 = guidance.numpy_to_pil(x0)[0]
-            x0.save('./test_imgs/x0_sr.png')
+            x0.save('./test_imgs/x0_fern_sr.png')
         if i % 1000 == 0 and i != 0:
             print(f'loss : {sum(grad_list) / len(grad_list)}')
             grad_list.clear()
